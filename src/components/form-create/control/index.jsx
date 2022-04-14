2432b0ff0e2772ce87846d4cb5b5e0dcbe3df86e
hhq <13486485935@163.com>
2022-04-14 11:24:28 +0800

    mod: form-create

--- src/components/form-create/control/index.jsx
+++ src/components/form-create/control/index.jsx
@@ -1,46 +1,26 @@
 import { ReactSortable } from "react-sortablejs";
-import Sortable from 'sortablejs';
 import './index.scss'
-import { useEffect } from "react";
-
-const list = [
-  { key: 'Text', name: '文本' },
-  { key: 'Input', name: '输入框' },
-  { key: 'InputNumber', name: '数字输入框' },
-  { key: 'Select', name: '选择器' },
-  { key: 'Radio', name: '单选框' },
-  { key: 'Checkbox', name: '复选框' },
-  { key: 'CheckboxGroup', name: '复选框组' },
-  { key: 'Switch', name: '开关' },
-  { key: 'TimePicker', name: '时间选择器' },
-  { key: 'DatePicker', name: '日期选择器' },
-  { key: 'Rate', name: '评分' },
-  { key: 'Slider', name: '滑动输入条' },
-  { key: 'TreeSelect', name: '树选择' },
-  { key: 'Cascader', name: '级联选择' },
-  { key: 'Upload', name: '上传' },
-]
+import {useState} from "react";
+import { list as _list, createId } from '../util'
 
 function Controls(props) {
 
-  useEffect(() => {
-    const el = document.getElementById('items');
-    Sortable.create(el, {
-      group: {
-        name: 'formCreate',
-        pull: 'clone',
-        put: false
-      },
-      sort: false
-    });
-  }, [])
+  const [list, setList] = useState(_list);
 
   return <div className="form-create-controls">
-    <div id="items">
-      {
-        list.map((item) => <div key={item.key} className="item">{ item.name }</div>)
-      }
-    </div>
+    <ReactSortable
+      id="control-items"
+      list={list}
+      setList={setList}
+      animation={150}
+      group={{ name: "form-create", pull: "clone", put: false }}
+      clone={item => ({ ...item, id: createId() })}
+      sort={false}
+    >
+      {list.map(item => (
+        <div key={item.id} className="item">{ item.name }</div>
+      ))}
+    </ReactSortable>
   </div>
 }
 
--- src/components/form-create/layout/index.jsx
+++ src/components/form-create/layout/index.jsx
@@ -1,41 +1,23 @@
-import {useEffect, useState} from 'react'
+import { useState } from 'react'
 import { ReactSortable } from "react-sortablejs";
 import './index.scss'
-import Sortable from "sortablejs";
-
-const list = [
-  { key: 'Text', name: '文本' },
-  { key: 'Input', name: '输入框' },
-  { key: 'InputNumber', name: '数字输入框' },
-  { key: 'Select', name: '选择器' },
-  { key: 'Radio', name: '单选框' },
-  { key: 'Checkbox', name: '复选框' },
-  { key: 'CheckboxGroup', name: '复选框组' },
-  { key: 'Switch', name: '开关' },
-  { key: 'TimePicker', name: '时间选择器' },
-  { key: 'DatePicker', name: '日期选择器' },
-  { key: 'Rate', name: '评分' },
-  { key: 'Slider', name: '滑动输入条' },
-  { key: 'TreeSelect', name: '树选择' },
-  { key: 'Cascader', name: '级联选择' },
-  { key: 'Upload', name: '上传' },
-]
 
 function Layout(props) {
 
-  useEffect(() => {
-    const el = document.getElementById('aaa');
-    Sortable.create(el, {
-      group: 'formCreate',
-    });
-  }, [])
+  const [list, setList] = useState([]);
 
   return <div className="form-create-layout">
-    <div id="aaa">
-      {list.map((item, index) => (
-        <div key={index}>{item.name}</div>
+    <ReactSortable
+      className="drag-items"
+      list={list}
+      setList={setList}
+      animation={150}
+      group={{ name: "form-create", pull: "clone" }}
+    >
+      {list.map(item => (
+        <div key={item.id}>{item.name}</div>
       ))}
-    </div>
+    </ReactSortable>
   </div>
 }
 
