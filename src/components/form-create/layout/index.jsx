6b69fe45024f9136999f8feb1d30fb1c4a3ff2f2
hhq <13486485935@163.com>
2022-04-29 17:53:05 +0800

    mod: 重新修改方案，使用 引入 Sortable，不使用 hooks形式，使用class组件形式

--- src/components/form-create/layout/index.jsx
+++ src/components/form-create/layout/index.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import {ReactSortable} from "react-sortablejs";
+import React, {Component} from 'react'
+import Sortable from 'react-sortablejs';
 import _, { uniqueId } from 'lodash';
 import './index.scss';
 import { Form, Row, Col } from 'antd';
@@ -23,73 +23,78 @@ const Controls = {
   rate: Rate
 }
 
-function DragFormLayout(props) {
+const sortableOption = {
+  animation: 150,
+  // fallbackOnBody: true,
+  // swapThreshold: 0.65,
+  handle: '.drag-wrapper--drag-icon',
+  group: {
+    name: 'form-create',
+    pull: true,
+    put: true,
+  },
+}
 
-  const [list, setList] = useState([]);
-  const [selected, setSelected] = useState(undefined)
+class DragFormLayout extends Component {
 
-  const [form] = Form.useForm();
+  formRef = React.createRef()
 
-  const sortableAdd = (evt) => {
+  state = {
+    list: [],
+    selected: undefined
+  }
+
+  // 添加
+  sortableAdd = (evt) => {
     console.log('Add', evt)
   }
 
-  const sortableUpdate = (evt) => {
+  // 更新
+  sortableUpdate = (evt) => {
     console.log('Update', evt)
   }
 
-  const onDragWrapperClick = (item) => {
-    setSelected(item.id)
+  // 点击移动容器
+  onDragWrapperClick = (item) => {
+    // setSelected(item.id)
   }
 
   // 容器拷贝（无关数据）
-  const onDragWrapperAdd = (item, index) => {
-    const _list = _.cloneDeep(list)
-    _list.splice(index+1, 0, {...item, id: uniqueId('field_')})
-    setList(_list)
+  onDragWrapperAdd = (item, index) => {
+    // const _list = _.cloneDeep(list)
+    // _list.splice(index+1, 0, {...item, id: uniqueId('field_')})
+    // setList(_list)
   }
 
   // 容器拷贝（包括数据）
-  const onDragWrapperCopy = (item, index) => {
-    console.log(form.getFieldsValue())
+  onDragWrapperCopy = (item, index) => {
+    // console.log(formRef.getFieldsValue())
     // const _list = _.cloneDeep(list)
     // _list.splice(index+1, 0, {...item, id: uniqueId('field_')})
     // setList(_list)
   }
 
   // 容器删除
-  const onDragWrapperDelete = (item, index) => {
-    setList(list.filter(_item => _item.id !== item.id))
+  onDragWrapperDelete = (item, index) => {
+    // setList(list.filter(_item => _item.id !== item.id))
   }
 
-  const loop = (arr=[], level='') => arr.map((item, index) => {
+  loop = (arr=[], level='') => arr.map((item, index) => {
     // 层级 例：0-1  1-1-1
     let _level = level==='' ? index + '' : `${level}-${index}`
 
     let RenderCom
     if(item.type === 'container') {  // 如果类型是容器
-      RenderCom = <ReactSortable
+      RenderCom = <Sortable
         className="loop-drag-items"
-        animation={150}
-        handle=".drag-wrapper--drag-icon"
-        group={{ name: 'form-create', pull: true, put: true }}
-        list={item.children}
-        setList={(...args) => {
-          console.log(2, args)
-          const _list = _.cloneDeep(args[0])
-          _list.forEach(item => {
-            if(!item.id) {
-              item.id = uniqueId('field_')
-            }
-          })
-          item.children = _list
-          setList(_.cloneDeep(list))
+        options={{
+          ...sortableOption,
+          onAdd: evt => (this.sortableAdd(evt)),
+          onUpdate: evt => (this.sortableUpdate(evt)),
         }}
-        onAdd={sortableAdd}
-        onUpdate={sortableUpdate}
       >
-        {loop(item.children, _level)}
-      </ReactSortable>
+        {this.loop(item.children, _level)}
+      </Sortable>
     } else if(item.type === 'grid') {  // 如果类型是栅格
       RenderCom = <Row
         className="grid-form-item-row"
@@ -117,45 +122,36 @@ function DragFormLayout(props) {
     return <DragWrapper
       key={_level}
       level={_level}
-      selected={selected === item.id}
-      onClick={() => onDragWrapperClick(item, index)}
-      onAdd={() => onDragWrapperAdd(item, index)}
-      onCopy={() => onDragWrapperCopy(item, index)}
-      onDelete={() => onDragWrapperDelete(item, index)}
+      selected={this.state.selected === item.id}
+      onClick={() => this.onDragWrapperClick(item, index)}
+      onAdd={() => this.onDragWrapperAdd(item, index)}
+      onCopy={() => this.onDragWrapperCopy(item, index)}
+      onDelete={() => this.onDragWrapperDelete(item, index)}
     >
       { RenderCom }
     </DragWrapper>
   })
 
-  return <div className="drag-form-layout">
-    <Form
-      className="drag-form"
-      form={form}
-      name="drag-form"
-    >
-      <ReactSortable
-        className="drag-items"
-        animation={150}
-        handle=".drag-wrapper--drag-icon"
-        group={{name: "form-create"}}
-        list={list}
-        setList={(...args) => {
-          console.log(1, args)
-          const _list = _.cloneDeep(args[0])
-          _list.forEach(item => {
-            if(!item.id) {
-              item.id = uniqueId('field_')
-            }
-          })
-          setList(_list)
-        }}
-        onAdd={sortableAdd}
-        onUpdate={sortableUpdate}
+  render() {
+    return <div className="drag-form-layout">
+      <Form
+        className="drag-form"
+        form={this.formRef}
+        name="drag-form"
       >
-        {loop(list)}
-      </ReactSortable>
-    </Form>
-  </div>
+        <Sortable
+          className="drag-items"
+          options={{
+            ...sortableOption,
+            onAdd: evt => (this.sortableAdd(evt)),
+            onUpdate: evt => (this.sortableUpdate(evt)),
+          }}
+        >
+          {this.loop(this.state.list)}
+        </Sortable>
+      </Form>
+    </div>
+  }
 }
 
 export default DragFormLayout
---
+++ src/components/form-create/layout/index1.jsx
@@ -0,0 +1,161 @@
+import React, {useState} from 'react'
+import {ReactSortable} from "react-sortablejs";
+import _, { uniqueId } from 'lodash';
+import './index.scss';
+import { Form, Row, Col } from 'antd';
+import { Text, Input, InputNumber, Select, TimePicker, DatePicker, Cascader, TreeSelect, Switch, Slider, RadioGroup, Checkbox, CheckboxGroup, Rate } from '@/packages/form/components'
+import DragWrapper from "./components/DragWrapper"
+
+const Controls = {
+  text: Text,
+  input: Input,
+  number: InputNumber,
+  select: Select,
+  time: TimePicker,
+  date: DatePicker,
+  cascader: Cascader,
+  treeselect: TreeSelect,
+  switch: Switch,
+  slider: Slider,
+  radiogroup: RadioGroup,
+  checkbox: Checkbox,
+  checkboxgroup: CheckboxGroup,
+  rate: Rate
+}
+
+function DragFormLayout(props) {
+
+  const [list, setList] = useState([]);
+  const [selected, setSelected] = useState(undefined)
+
+  const [form] = Form.useForm();
+
+  const sortableAdd = (evt) => {
+    console.log('Add', evt)
+  }
+
+  const sortableUpdate = (evt) => {
+    console.log('Update', evt)
+  }
+
+  const onDragWrapperClick = (item) => {
+    setSelected(item.id)
+  }
+
+  // 容器拷贝（无关数据）
+  const onDragWrapperAdd = (item, index) => {
+    const _list = _.cloneDeep(list)
+    _list.splice(index+1, 0, {...item, id: uniqueId('field_')})
+    setList(_list)
+  }
+
+  // 容器拷贝（包括数据）
+  const onDragWrapperCopy = (item, index) => {
+    console.log(form.getFieldsValue())
+    // const _list = _.cloneDeep(list)
+    // _list.splice(index+1, 0, {...item, id: uniqueId('field_')})
+    // setList(_list)
+  }
+
+  // 容器删除
+  const onDragWrapperDelete = (item, index) => {
+    setList(list.filter(_item => _item.id !== item.id))
+  }
+
+  const loop = (arr=[], level='') => arr.map((item, index) => {
+    // 层级 例：0-1  1-1-1
+    let _level = level==='' ? index + '' : `${level}-${index}`
+
+    let RenderCom
+    if(item.type === 'container') {  // 如果类型是容器
+      RenderCom = <ReactSortable
+        className="loop-drag-items"
+        animation={150}
+        handle=".drag-wrapper--drag-icon"
+        group={{ name: 'form-create', pull: true, put: true }}
+        list={item.children}
+        setList={(...args) => {
+          console.log(2, args)
+          const _list = _.cloneDeep(args[0])
+          _list.forEach(item => {
+            if(!item.id) {
+              item.id = uniqueId('field_')
+            }
+          })
+          item.children = _list
+          setList(_.cloneDeep(list))
+        }}
+        onAdd={sortableAdd}
+        onUpdate={sortableUpdate}
+      >
+        {loop(item.children, _level)}
+      </ReactSortable>
+    } else if(item.type === 'grid') {  // 如果类型是栅格
+      RenderCom = <Row
+        className="grid-form-item-row"
+      >
+        {
+          item.children.map((col, _index) => <Col
+            key={_index}
+            className="grid-form-item-col"
+            span={24/item.children.length}
+          >
+            1111
+          </Col>)
+        }
+      </Row>
+    } else { // 其他类型
+      const Com = Controls[item.type.toLowerCase()]
+      RenderCom = Com ? <Form.Item
+        name={item.id}
+        label={item.id}
+      >
+        <Com {...item.attr} />
+      </Form.Item> : <div />
+    }
+
+    return <DragWrapper
+      key={_level}
+      level={_level}
+      selected={selected === item.id}
+      onClick={() => onDragWrapperClick(item, index)}
+      onAdd={() => onDragWrapperAdd(item, index)}
+      onCopy={() => onDragWrapperCopy(item, index)}
+      onDelete={() => onDragWrapperDelete(item, index)}
+    >
+      { RenderCom }
+    </DragWrapper>
+  })
+
+  return <div className="drag-form-layout">
+    <Form
+      className="drag-form"
+      form={form}
+      name="drag-form"
+    >
+      <ReactSortable
+        className="drag-items"
+        animation={150}
+        handle=".drag-wrapper--drag-icon"
+        group={{name: "form-create"}}
+        list={list}
+        setList={(...args) => {
+          console.log(1, args)
+          const _list = _.cloneDeep(args[0])
+          _list.forEach(item => {
+            if(!item.id) {
+              item.id = uniqueId('field_')
+            }
+          })
+          setList(_list)
+        }}
+        onAdd={sortableAdd}
+        onUpdate={sortableUpdate}
+      >
+        {loop(list)}
+      </ReactSortable>
+    </Form>
+  </div>
+}
+
+export default DragFormLayout
